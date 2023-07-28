a6f3692b22057ddbe1855ed1a11f8b43b40e4346
DHEYA97 <Eng.DhiaAljabobi@gmail.com>
2023-07-28 16:15:11 +0300

    11 commit

--- js/js.js
+++ js/js.js
@@ -100,8 +100,8 @@ if(login)
             {
                 userSesstionList.push(user[currentIndex])
                 localStorage.setItem('userSesstion',JSON.stringify(userSesstionList))
-                // console.log(userSesstionList);
-                location.replace('https://' + location.hostname  + "/" + baselocation + "/home.html")
+                console.log(baselocation);
+                // location.replace('https://' + location.hostname  + "/" + baselocation + "/home.html")
                 // if (baselocation == '/') {
                 //     location.replace('https://' + location.hostname + '/home.html')
         
